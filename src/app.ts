4b60ecfff7f52d7e6d1d6c86b77ebcb22e0097c2
matrecek1 <grenovec@seznam.cz>
2023-02-11 12:26:29 +0100

    env variables setting

--- src/app.ts
+++ src/app.ts
@@ -4,12 +4,17 @@ import { ExpressError } from "./utils/expressError";
 import boulderRoutes from "./routes/gym/boulders.route"
 import gymRoutes from "./routes/gym/gym.route"
 import wallRoutes from "./routes/gym/wall.route"
+import * as dotenv from 'dotenv'
+dotenv.config({ path: '../../.env' })
+
+const port = process.env.PORT || '3000'
+const mongoUrl = process.env.MONGO_URL || 'mongodb://127.0.0.1:27017/BoulderApp'
 
 const app = express();
 
 async function main() {
     mongoose.set('strictQuery', false)
-    await mongoose.connect("mongodb://127.0.0.1:27017/BoulderApp");
+    await mongoose.connect(mongoUrl);
     console.log(`connected to db`);
 }
 main().catch(err => console.log(err));
@@ -34,8 +39,8 @@ app.use(function (err: Error, req: Request, res: Response, next: NextFunction) {
 });
 
 
-app.listen('3000', () => {
-    console.log(`Listening on port 3000, Url: http://localhost:3000`);
+app.listen(port, () => {
+    console.log(`Listening on port ${port}, Url: http://localhost:${port}`);
 });
 
 
